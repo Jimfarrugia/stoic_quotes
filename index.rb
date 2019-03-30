341665cc45a07141bcb02d7629daeb797a42df23
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-03-30 19:19:37 +1100

    created Request class with #quotes method to call from quotes api.

--- index.rb
+++ index.rb
@@ -1,1 +1,22 @@
+require 'rubygems'
 require 'httparty'
+
+# API
+# https://randomstoicquotesapi.herokuapp.com/
+# API Docs
+# https://github.com/jimmerioles/random-stoic-quotes-api
+
+class Request
+  include HTTParty
+  base_uri "randomstoicquotesapi.herokuapp.com"
+
+  def quotes
+    self.class.get('/api/v1/quotes')
+  end
+end
+
+request = Request.new
+
+request.quotes.each do |item|
+  puts item
+end
