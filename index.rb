97a394425d9a6cf9435bbb1f0094efc5378ed0c3
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-03-30 19:44:40 +1100

    added class instance variable and getter for @num_of_quotes.

--- index.rb
+++ index.rb
@@ -14,19 +14,24 @@ class StoicQuotes
 
   def initialize
     @request = self.class.get('/api/v1/quotes')["data"]
+    @num_of_quotes = @request.length
+  end
+
+  def num_of_quotes
+    @num_of_quotes
   end
   
   def first_quote
     @request[0]["attributes"]["text"]
   end
 
-  
 
 end
 
 stoic_quotes = StoicQuotes.new
 
 puts stoic_quotes.first_quote
+puts stoic_quotes.num_of_quotes
 #request.quotes.each do |item|
 #  p item
 #  puts "\n\n"
